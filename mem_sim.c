fc456b25bb97b8f0b1c35f43c3f3460fe5244653
Netanel Draiman <ndraiman@gmail.com>
2015-06-08 23:29:07 +0300

    Initial Ex5 Version

---
+++ mem_sim.c
@@ -0,0 +1,127 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h> 
+#include <fcntl.h>
+#include "mem_sim.h"
+
+/*****************************
+ * SYSTEM:
+ * 12 bit system - 12 bit per pointer
+ * virtual memory = 2^12 = 4096 bit 
+ * 7 msb = page number - 2^7 = 128 pages
+ * 5 lsb = offset in page - size of page: 2^5 = 32 bit 
+ *****************************/
+
+#define SWAP_SIZE 4096
+#define FRAME_SIZE 32
+#define PAGE_SIZE 32
+#define MEMORY_SIZE 512
+
+typedef struct page_descriptor {
+  unsigned int valid; //valid bit - 1=in main memory, 0=in swapfile\executable
+  unsigned int permission; // 0=READ & WRITE, 1=READ ONLY
+  unsigned int dirty; // 1=data was written to page, 0=no data was written
+  unsigned int frame; // page's frame # in main memory (if valid=1)
+  
+  int ref_bit; //reference bit for LRU algorithm
+  
+}page_descriptor_t;
+
+struct sim_database {
+  page_descriptor_t* page_table; //pointer to page page_table
+  char* swapfile_name; //name of swap file
+  int swapfile_fd; //swap file fd
+  char* executable_name; //name of executable file
+  int executable_fd; //executable file fd
+};
+
+int bitmap[16];
+
+int numOfPages = SWAP_SIZE/PAGE_SIZE; //128 pages
+int exec_size; // save file size - used to determine beginning of heap\stack in VM
+
+static char* RAM[MEMORY_SIZE]; //each 32 bit is a frame. total: 16 frames
+
+
+
+sim_database_t* vm_constructor(char *executable, int text_size, int data_size, int bss_size){
+  
+  if(executable == NULL)
+    return NULL;
+  
+  sim_database_t* db = (sim_database_t*)malloc(sizeof(sim_database_t));
+  if(db == NULL) {
+    perror("ERROR: failed to allocate memory for sim_database\n");
+    return NULL;
+  }
+  
+  db->page_table = (page_descriptor_t*)malloc(sizeof(page_descriptor_t)*(numOfPages));
+  if(db->page_table == NULL) {
+    perror("ERROR: failed to allocate memory for page_table\n");
+    free(db);
+    return NULL;
+  }
+  
+  
+  db->executable_name = executable;
+  if( (db->executable_fd = open(db->executable_name, O_RDONLY, 0777)) < 0) {
+    perror("ERROR: failed to open executable\n");
+    free(db->page_table);
+    free(db);
+    return NULL;
+  }
+
+  
+  int i;
+  for(i=0; i < numOfPages; i++) {
+    if(i < text_size)
+      db->page_table[i].permission = 1; //READ ONLY
+    else
+      db->page_table[i].permission = 0; //READ & WRITE
+      
+    db->page_table[i].valid = 0;
+    db->page_table[i].dirty = 0;
+    db->page_table[i].frame = -1;
+    db->page_table[i].ref_bit = 0;
+  }
+  
+  db->swapfile_name = "swapfile";
+  if( (db->swapfile_fd = open(db->swapfile_name, O_RDWR | O_CREAT, 0777)) < 0) {
+    perror("ERROR: failed to create swap file\n");
+    free(db->page_table);
+    free(db);
+    return NULL;
+  }
+  
+  exec_size = text_size + bss_size + data_size; //assign global variable
+  
+  return db;
+}
+
+
+
+int vm_load(sim_database_t *sim_db, unsigned short address, unsigned char *p_char) {
+  int page, offset, frame;
+  page = address >> 5; //shift number 5 bits to get page number
+  offset = address & (PAGE_SIZE-1); // mask 5 LSBs to get offset number 
+  
+  //if in RAM
+  if (sim_db->page_table[page].valid) { //!= 0 
+    sim_db->page_table[page].ref_bit = 1;
+    frame = sim_db->page_table[page].frame;
+    p_char = RAM[frame * PAGE_SIZE + offset];
+    return 0;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+
